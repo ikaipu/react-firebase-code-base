ea132887c2ae1ac7ae33495ae6ebbad86f3d8204
vananaz_takmin <ikaipu@vananaz.com>
2020-11-17 11:49:14 +0800

    demo

--- src/components/atoms/A/A.tsx
+++ src/components/atoms/A/A.tsx
@@ -8,7 +8,7 @@ export interface Props {
 const A: FC<Props> = ({ children, onClick = () => null }) => {
   return (
     <button className="link-button" type="button" onClick={onClick}>
-      {children}
+      Test {children}
     </button>
   );
 };
